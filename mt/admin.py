3b10ef5499fe28207ee54bc3d4a311a70f29d907
Sebastin Santy <t-sesant@microsoft.com>
2020-01-13 11:03:21 +0000

    Merged PR 6: Add mouse suggestion selection functionality

--- mt/admin.py
+++ mt/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import *
 from django.apps import apps
+from django.http import HttpResponse
+import csv
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+        
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+# @admin.register(dockeystroke)
+class CSVAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_per_page = 2000
+    actions = ["export_as_csv"]
+
+# @admin.register(translatedSentence)
+# class TranslAdmin(admin.ModelAdmin, ExportCsvMixin):
+#     actions = ["export_as_csv"]
 
 for model in apps.get_app_config('mt').models.values():
-    admin.site.register(model)
+    admin.site.register(model, CSVAdmin)
